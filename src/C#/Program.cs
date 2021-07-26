a90e371bca639b3f724bfa6644ce4adbeca119ee
Akna Mafaid Ilmi <aknaroyal@gmail.com>
2021-07-26 13:16:41 +0800

    perubahan lain untuk di-push ke Github

---
+++ src/C#/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Halo, ini adalah koding tertanggal : " + DateTime.Now);
+            // input
+            int a = 0;
+
+            //output
+            Console.WriteLine(a??3);
+            Console.ReadKey();
+        }
+    }
+}
